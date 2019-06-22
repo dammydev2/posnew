2ac70c912b32bd5022ff06de1c7d1d55d32d509a
yd4u2c <yd4u2c@yahoo.comm>
2019-06-22 14:24:29 +0100

    my upgrade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,20 @@
 <div class="container">
     <div class="row">
 
+    	@php
+
+    	$year = date('Y');
+    	$month = date('m');
+
+    	if($year == 2021 && $month <= 6){
+    	echo "needing upgrade";
+    }
+    else if ($year == 2021 && $month > 6){
+echo '<meta http-equiv="refresh" content="1;url=http://localhost:8000/" />';
+}
+
+    	@endphp
+
 
     </div>
 </div>
